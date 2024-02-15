f1dcba9c702db4c2506edfdab7112a4c85c4824e
Charles <charlescon@live.com>
2024-02-15 16:38:03 -0600

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,194 @@
+let mic;
+let angle = 0;
+let test = 0;
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    let btns = document.getElementsByClassName("navBtnCont")
+    let hideBtn = document.getElementById("hide");
+    let showBtn = document.getElementById("show");
+    hideBtn.addEventListener("click", (e) => {
+        btns.forEach( e => {
+            e.classList.toggle('hidden')
+        })
+    })
+    showBtn.addEventListener("click", (e) => {
+        btns.forEach( e => {
+            e.classList.toggle('hidden')
+        })
+    })
+});
+
+function preload() {
+    song = loadSound("./assets/music/mitski.mp3");
+}
+
+function togglePlay() {
+    if (song.isPlaying()) {
+        song.pause();
+    } else {
+        song.loop();
+    }
+}
+
+function setup(){
+    let cnv = createCanvas(windowWidth, windowHeight);
+    cnv.mousePressed(togglePlay);
+    textAlign(CENTER);
+    mic = new p5.AudioIn();
+    mic.start();
+    fft = new p5.FFT(.8, 1024);
+    fft.setInput();
+}
+
+function draw(){
+    background(0, 0, 0, 255);
+    //text('tap to start', width/2, 20);
+
+    let spectrum = fft.analyze();
+    let b = Math.floor(fft.getEnergy("lowMid"));
+    let g = Math.floor(fft.getEnergy("mid"));
+    let r = Math.floor(fft.getEnergy("highMid"));
+
+    let cent = Math.floor(fft.getCentroid());
+    let centroid = Math.floor(map(cent, 0, 10000, 5, 1))
+    // fill(255)
+    // text(`${centroid}`, width/2, 20)
+
+    // RECTANGLE
+    // noStroke();
+    // beginShape();
+    // push();
+    // translate(width / 2, height / 2);
+    // angle += radians(.25);
+    // rotate(angle);
+    // for (let i = 0; i< spectrum.length-350; i++){
+    //     let y = map(i, 0, spectrum.length-350, 0, -height*1.5);
+    //     let y2 = map(i, spectrum.length-350, 0, height*1.5, 0);
+    //     let x = map(i, 0, spectrum.length-350, 0, -width*1.5);
+    //     let x2 = map(i, spectrum.length-350, 0, width*1.5, 0);
+
+    //     let h = -height + map(spectrum[i], 0, 255, height, 0);
+
+    //     fill(`rgba( 0, ${spectrum[i]}, ${spectrum[i]}, .5)`)
+    //     rect(-width, y, width*2, 2 )
+    //     rect(-width, y2, width*2, 2 )
+    //     rect(x, -height*1.5, 2, height*4 )
+    //     rect(x2, -height*1.5, 2, height*4 )
+    // }
+    // pop();
+    // endShape();
+
+    // RECTANGLE OPPOSITE DIRECTION
+    // noStroke();
+    // translate(width / 2, height / 2);
+    // angle += radians(.25);
+    // rotate(-angle);
+    // for (let i = 0; i< spectrum.length-350; i++){
+    //     let y = map(i, 0, spectrum.length-350, 0, -height*1.5);
+    //     let y2 = map(i, spectrum.length-350, 0, height*1.5, 0);
+    //     let x = map(i, 0, spectrum.length-350, 0, -width*1.5);
+    //     let x2 = map(i, spectrum.length-350, 0, width*1.5, 0);
+
+    //     let h = -height + map(spectrum[i], 0, 255, height, 0);
+
+    //     fill(`rgba( 0, ${spectrum[i]}, ${spectrum[i]}, .25)`)
+    //     rect(-width, y, width*2, 2 )
+    //     rect(-width, y2, width*2, 2 )
+    //     rect(x, -height*1.5, 2, height*4 )
+    //     rect(x2, -height*1.5, 2, height*4 )
+    // }
+    
+
+
+
+    
+    // VERTICAL FREQUENCY BARS
+    // beginShape();
+    // noStroke();
+    // push();
+    // translate(width / 2, height / 2);
+    // // angle += radians(.25);
+    // // rotate(angle);
+    // for (let i = 0; i< spectrum.length-350; i++){
+    //     let x = map(i, 0, spectrum.length-350, 0, -width*1.5);
+    //     let x2 = map(i, spectrum.length-350, 0, width*1.5, 0);
+
+    //     let h = -height + map(spectrum[i], 0, 255, height, 0);
+
+    //     fill(`rgba(${spectrum[i]},${spectrum[i]},${spectrum[i]}, .25)`)
+    //     rect(x, -height*1.5, 2, height*4 )
+    //     rect(x2, -height*1.5, 2, height*4 )
+        
+    // }
+    // endShape();
+    // pop();
+
+
+    // HORIZONTAL FREQUENCY LINES
+    // noStroke();
+    // push();
+    // translate(width / 2, height / 2);
+    // // angle += radians(.25);
+    // // rotate(-angle);
+    // test += 1;
+    // for (let i = 0; i< spectrum.length-350; i++){
+    //     let y = map(i, 0, spectrum.length-350, 0, -height*1.5);
+    //     let y2 = map(i, spectrum.length-350, 0, height*1.5, 0);
+
+    //     let h = -height + map(spectrum[i], 0, 255, height, 0);
+
+    //     fill(`rgba(${spectrum[i]},${spectrum[i]},${spectrum[i]}, .25)`)
+    //     rect(-width, y, width*2, 1 )
+    //     rect(-width, y2, width*2, 1 )
+        
+    // }
+    // pop();
+    
+    
+
+    let waveform = fft.waveform();
+
+    // WAVEFORM
+    // beginShape();
+    // noFill()
+    // stroke(255, 255, 255);
+    // strokeWeight(5);
+    
+    // for (let i = 0; i < waveform.length; i++){
+    //     let x = map(i, 0, waveform.length/2, 0, width/2);
+    //     let y = map( waveform[i], -1, 1, 0, height);
+    //     vertex(x,y);
+        
+    // }
+    // endShape();
+
+
+
+    // BLACK HOLE VISUAL
+    noStroke();
+    beginShape();
+    push();
+    translate(width / 2, height / 2);
+    angle += radians(1);
+    //rotate(-angle/centroid);
+    for (let i = 0; i< spectrum.length-350; i++){
+        let y = map(i, 0, spectrum.length-350, height/2, height);
+        let y2 = map(i, spectrum.length-350, 0, 0, height/2);
+        let x = map(i, 0, spectrum.length-350, width/2, width);
+        let x2 = map(i, spectrum.length-350, 0, 0, width/2);
+
+        let h = -height + map(spectrum[i], 0, 255, height, 0);
+        //stroke(r,g,b)
+        //fill(spectrum[i], spectrum[i], spectrum[i])
+        fill(r, g, b, spectrum[i])
+        rect(0, y, width, 1 )
+        rect(0, y2, width, 1 )
+        rect(x, 0, 1, height )
+        rect(x2, 0, 1, height )
+        //angle += radians(1);
+        rotate(angle/200);
+    }
+    pop();
+    endShape();
+}
+  
---
+++ assets/js/main2.js
@@ -0,0 +1,159 @@
+// let song;
+// let amp;
+// let mic;
+// let phase = 0;
+// let zoff = 0;
+// let spaceSlider;
+// let strokeSlider;
+// var stars = [];
+// var starSpeed;
+// let rToggle = 1;
+// let r;
+// let g;
+// let b;
+
+// // function preload() {
+// //     song = loadSound("daftPunk.mp3");
+// // }
+
+// function toggleSong() {
+//     if (song.isPlaying()) {
+//         song.pause();
+//         document.getElementById("playImg").src = "assets/images/playBtn.png";
+//     } else {
+//         song.play();
+//         document.getElementById("playImg").src = "assets/images/pauseBtn.png";
+//     }
+// }
+// function toggleSettings() {
+//     let controls = document.getElementById("controls");
+//     controls.classList.toggle("animate__fadeInRight");
+//     controls.classList.toggle("hide");
+// }
+// function toggleInfo() {
+//     let info = document.getElementById("infoBlob");
+//     info.classList.toggle("animate__fadeIn");
+//     info.classList.toggle("hide");
+// }
+// function toggleMusic() {
+//     let music = document.getElementById("musicChoice");
+//     music.classList.toggle("animate__fadeInLeft");
+//     music.classList.toggle("hide");
+// }
+// function changeSong() {
+//     if (song.isPlaying()) {
+//         song.pause();
+//         document.getElementById("playImg").src = "assets/images/playBtn.png";
+//     }
+//     song = loadSound(`${this.id}.mp3`);
+// }
+
+// // Perlin Noise Setup
+// function setup() {
+//     var initCanvas = createCanvas(windowWidth-100, windowHeight-100);
+//     initCanvas.parent("visuals");
+//     initCanvas.mousePressed(userStartAudio);
+//     // for (var i = 0; i < 800; i++) {
+//     //     stars[i] = new Star();
+//     // }
+//     // let btn = document.getElementById("playBtn");
+//     // btn.addEventListener('click', toggleSong);
+
+//     // let settings = document.getElementById("controlsCont");
+//     // settings.addEventListener('click', toggleSettings);
+
+//     // let musicCont = document.getElementById("musicBtn");
+//     // musicCont.addEventListener('click', toggleMusic);
+
+//     // let infoCont = document.getElementById("infoCont");
+//     // infoCont.addEventListener('click', toggleInfo);
+
+//     // let disco = document.getElementById("disco");
+
+//     // let daft = document.getElementById("daftPunk");
+//     // let kiss = document.getElementById("kiss");
+//     // let alice = document.getElementById("river");
+//     // let solitude = document.getElementById("mientras");
+//     // let death = document.getElementById("getGot");
+//     // daft.addEventListener('click', changeSong);
+//     // kiss.addEventListener('click', changeSong);
+//     // alice.addEventListener('click', changeSong);
+//     // solitude.addEventListener('click', changeSong);
+//     // death.addEventListener('click', changeSong);
+    
+//     amp = new p5.Amplitude();
+//     fft = new p5.FFT();
+//     spaceSlider = createSlider(.5, 15, .1, .1);
+//     strokeSlider = createSlider(1, 10, 2, .5);
+//     numSlider = createSlider(1, 5, 1, 1);
+//     qSlider = createSlider(1.1, 3, 1.5, .1);
+
+//     // spaceSlider.parent("spacing");
+//     // strokeSlider.parent("thickness");
+//     // numSlider.parent("dupes");
+//     // qSlider.parent("deviation");
+
+//     if (windowWidth < 600) {
+//         rToggle = .5;
+//     }
+// }
+
+// // draw using Perlin Noise
+// function draw() {
+//     text('tap to start', width/2, 120);
+//     // if (disco.checked) {
+//     //     r = Math.floor(Math.random()*255)+100
+//     //     g = Math.floor(Math.random()*255)+100
+//     //     b = Math.floor(Math.random()*255)+100
+//     // }
+//     // else {
+//     //     r = 255;
+//     //     g = 255;
+//     //     b = 255;
+//     // }
+//     let vol = amp.getLevel();
+//     let fre = fft.analyze();
+//     let centroid = fft.getCentroid();
+//     background(0);
+//     translate(width / 2, height / 2);
+//     stroke(r, g, b);
+//     let w = strokeSlider.value();
+//     beginShape(POINTS);
+//     strokeWeight(w);
+//     let spacing = spaceSlider.value();
+//     let circleNum = numSlider.value();
+//     let qNum = qSlider.value();
+//     let q = 500;
+
+//     for (let a = 0; a < TWO_PI; a += radians(spacing/2)) {
+//         let xoff = map(cos(a + phase), -1, 1, 0, (centroid/q));
+//         let yoff = map(sin(a + phase), -1, 1, 0, (centroid/q));
+//         let r = map(noise(xoff, yoff, zoff), 0, 1, 100, height / 2);
+//         let x = (r * rToggle) * cos(a);
+//         let y = (r * rToggle) * sin(a);
+//         vertex(x, y);
+//     }
+
+//     if(circleNum>1) {
+//         for (let k=1; k<circleNum; k++) {
+//             for (let a = 0; a < TWO_PI; a += radians(spacing/2)) {
+//                 let xoff = map(cos(a + phase), -1, 1, 0, (centroid/q));
+//                 let yoff = map(sin(a + phase), -1, 1, 0, (centroid/q));
+//                 let r = map(noise(xoff, yoff, zoff), 0, 1, 100, height / 2);
+//                 let x = (r * rToggle) * cos(a);
+//                 let y = (r * rToggle) * sin(a);
+//                 vertex(x, y);
+//             }
+//             q=q*qNum;
+//         }
+//     }
+//     endShape(CLOSE);
+//     // starSpeed = map(centroid/20, 0, width, 0, 50);
+//     // for (var i = 0; i < stars.length; i++) {
+//     //     strokeWeight(2);
+//     //     stars[i].update();
+//     //     stars[i].show();
+//     // }
+//     phase += (vol/4);
+//     zoff += (vol/8);
+// }
